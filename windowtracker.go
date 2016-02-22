0ad1dc3f102f56065ccea988050062850a8e5afc
Kevin P. Kucharczyk <kevinkucharczyk@gmail.com>
2016-02-22 17:40:05 +0100

    Save history to json file on exit

--- windowtracker.go
+++ windowtracker.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"os/exec"
+	"os/signal"
 	"strings"
 	"time"
 )
 
 var windows map[string]int64
+var signalChan chan os.Signal
 
 func doEvery(d time.Duration, f func(time.Time)) {
 	for x := range time.Tick(d) {
@@ -42,7 +46,30 @@ func printActiveWindow(t time.Time) {
 	fmt.Printf("%v: %v, total %v seconds\n", t.Format("2006-01-02 15:04:05 MST"), windowTitle, windows[windowTitle])
 }
 
+func beforeExit() {
+	<-signalChan
+	fmt.Println("Saving history to file...")
+	saveJsonFile(windows, "./history.json")
+	os.Exit(1)
+}
+
+func saveJsonFile(v interface{}, path string) {
+	fo, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+	defer fo.Close()
+	e := json.NewEncoder(fo)
+	if err := e.Encode(v); err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	windows = make(map[string]int64)
+	signalChan = make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	go beforeExit()
 	doEvery(time.Second, printActiveWindow)
 }
